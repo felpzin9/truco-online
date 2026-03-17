53a64ff3e7cf84d730d40e053b88f9b8ef9fb84e
felpzin9 <felipeff5894@gmail.com>
2026-03-17 19:08:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.get("/", (req, res) => {
+app.use((req, res) => {
   res.send(`
     <h1>Truco Online 🔥</h1>
 
